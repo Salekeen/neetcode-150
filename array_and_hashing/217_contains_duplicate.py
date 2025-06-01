0bf9796ce07388606c06a69c0d93c5ee32ff3652
salekeennayeem <nawaz.nayeem@selise.ch>
2025-06-01 09:27:24 +0600

    init commit

---
+++ array_and_hashing/217_contains_duplicate.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        pass
+
+
+def main():
+    s = Solution()
+    nums = [1, 2, 3, 1]
+    result = s.containsDuplicate(nums)
+    print(result)
